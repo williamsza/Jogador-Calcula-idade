952b30b957891ef67fd08166d6b8f0804d142cd7
williamsza <william.sza@outlook.com>
2023-01-13 15:13:46 -0300

    Atualização

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,37 +1,39 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Jogador {
     private String nome;
     private String nascionalidade;
     private String posicao;
     private double altura;
-    private LocalDate anoNascimento;
+    private LocalDate nascimento;
     private double peso;
 
     public Jogador(String nome, String nascionalidade, String posicao, double altura,
-            int i, double peso) {
+            LocalDate nascimento, double peso) {
         this.nome = nome;
         this.nascionalidade = nascionalidade;
         this.posicao = posicao;
-        this.anoNascimento = anoNascimento;
         this.altura = altura;
+        this.nascimento = nascimento;
         this.peso = peso;
     }
 
     public int calcularIdade() {
-        LocalDate hoje = LocalDate.now();
-        return (int) hoje.getYear() - this.anoNascimento.getYear();
+
+        LocalDate agora = LocalDate.now();
+        return (int) agora.getYear() - nascimento.getYear();
 
     }
 
-    public boolean imprimirDadosJogador() {
+    public void imprimirDados() {
         System.out.println("Nome do jogdor: " + this.getNome());
         System.out.println("Nascionalidade do jogador " + this.getNascionalidade());
+        System.out.println("Idade:" + this.calcularIdade());
         System.out.println("Posição do Jogador: " + this.getPosicao());
-        System.out.println("Data de nascimento do jogador " + this.getAnoNascimento());
+        System.out.println("Data de nascimento do jogador " + this.getNascimento());
         System.out.println("Altura do Jogador " + this.getAltura());
         System.out.println("Peso do Jogador " + this.getPeso());
-        return false;
 
     }
 
@@ -67,12 +69,12 @@ public class Jogador {
         this.altura = altura;
     }
 
-    public LocalDate getAnoNascimento() {
-        return anoNascimento;
+    public LocalDate getNascimento() {
+        return nascimento;
     }
 
-    public void setAnoNascimento(LocalDate anoNascimento) {
-        this.anoNascimento = anoNascimento;
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
     }
 
     public double getPeso() {
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,15 +1,17 @@
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class TestaMetodo {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Jogador pessoa = new Jogador("William", "Brasil", "meia", 1.74, 1981, 90);
-        
+		Jogador pessoa = new Jogador("William", "Brasileira", "meia", 1.74, LocalDate.of(1981, 7, 28), 90);
 
-        pessoa.calcularIdade();
-        pessoa.imprimirDadosJogador();
+		System.out.println(pessoa.calcularIdade());
 
-        System.out.print(pessoa.imprimirDadosJogador());
-        System.out.println(pessoa.calcularIdade());
-    }
+		//pessoa.imprimirDados();
+		pessoa.calcularIdade();
+
+	}
 }
